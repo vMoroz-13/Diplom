adc2c41d31c5120c750f9bfe5b76abd8aabdf682
Vadim Moroz <vmoroz13@yandex.ru>
2020-07-22 11:21:45 +0300

    Модульная структура

---
+++ src/index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+import 'nodelist-foreach-polyfill';
+import "@babel/polyfill";
+import elementClosest from 'element-closest';
+elementClosest(window);
+import 'formdata-polyfill';
+import 'es6-promise';
+import 'fetch-polyfill';
